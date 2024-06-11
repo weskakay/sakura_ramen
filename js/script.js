dce78d0a4ca6e22c5ab982f921a1bb5b9cbfeac0
Wessi <weskakay@gmail.com>
2024-06-11 11:13:03 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function switchLanguage(language) {
+  if (language === 'DE') {
+      window.location.href = 'index_de.html';
+  } else if (language === 'EN') {
+      window.location.href = 'index_en.html';
+  }
+}
